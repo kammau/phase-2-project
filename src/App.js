4380d1f6714114c59a9fabd66924d24ab6f384bf
Kamryn Smith <programersign@gmail.com>
2023-05-18 09:33:26 -0700

    added returns with different routes

--- src/App.js
+++ src/App.js
@@ -4,3 +4,22 @@ import Sell from './components/Sell';
 import Cart from './components/Cart';
 import Home from './components/Home';
 import { useEffect, useState } from 'react';
+
+function App() {
+    return (
+        <div>
+          <NavBar />
+          <Switch>
+            <Route exact path="/sell">
+              <Sell onHandleSubmit={handleSellBook}/>
+            </Route>
+            <Route exact path="/cart">
+              <Cart books={cart}/>
+            </Route>
+            <Route exact path="/">
+              <Home books={books} onAddToCart={handleAddToCart}/>
+            </Route>
+          </Switch>
+        </div>
+      );
+}
