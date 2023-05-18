7ab77e3688238861b3caec1a5ac70b36640a52f4
Kamryn Smith <programersign@gmail.com>
2023-05-18 10:17:18 -0700

    added book code

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,12 +1,24 @@
 import React from "react";
-import Book from "./Book";
 
-function Home({books, onAddToCart}) {
+function Book({book, onAddToCart}) {
+
+
+    function handleClick(book) {
+        if (book.stock === "Available") {
+            onAddToCart(book)
+        }
+    }
+
     return (
-        books.map((book) => {
-            return <Book key={book.id} book={book} onAddToCart={onAddToCart}/>
-        })
+        <div className="card" id={book.id}>
+            <h3>{book.title}</h3>
+            <p>{book.author}</p>
+            <img className="element_resize" src={book.image} alt={`${book.title} cover`}/>
+            <p style={book.stock === "Available" ? {color: "green"} : {color: "red"}}>{book.stock}</p> 
+            <p>${book.price}</p>
+            <button onClick={() => handleClick(book)} style={book.stock === "Available" ? {textDecoration: "none"} : {textDecoration: "line-through"}}>Add to Cart</button>
+        </div>
     )
 }
 
-export default Home;
+export default Book;
