7767b2e8788b4ff88e489592726401f7a2578f9e
Kamryn Smith <programersign@gmail.com>
2023-05-18 10:18:20 -0700

    added navBar code

---
+++ src/components/NavBar.js
@@ -0,0 +1,27 @@
+
+import React from "react";
+import {NavLink} from "react-router-dom";
+
+const linkStyles = {
+    text: "center",
+    display: "inline-block",
+    width: "50px",
+    padding: "12px",
+    margin: "0 6px 6px",
+    background: "gray",
+    textDecoration: "none",
+    color: "black",
+    font: "Abel",
+}
+
+function NavBar() {
+    return (
+        <div className="links">
+            <NavLink to="/" exact style={linkStyles}><img className="icon" src="https://freeiconshop.com/wp-content/uploads/edd/shopping-bag-outline.png"/>Shop</NavLink>
+            <NavLink to="/sell" exact style={linkStyles}><img className="icon" src="https://static.vecteezy.com/system/resources/previews/014/455/861/original/illustration-of-give-money-icon-on-transparent-background-free-png.png"/>Sell</NavLink>
+            <NavLink to="/cart" exact style={linkStyles}><img className="icon" src="https://cdn-icons-png.flaticon.com/512/263/263142.png" alt="icon of shopping cart"/>Cart</NavLink>
+        </div>
+    )
+}
+
+export default NavBar;
